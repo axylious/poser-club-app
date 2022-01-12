f27ff5f3f422b93db20849cfc62a230b31dfebc8
Axel Olivas <axelbrolivas@gmail.com>
2022-01-12 14:49:38 -0800

    testing dummy data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,30 +1,32 @@
 import { MoreVert } from '@material-ui/icons'
 import './post.css'
+import {Users} from '../../dummyData'
 
-export default function Post() {
+export default function Post({post}) {
    return (
       <div>
          <div className="post">
             <div className="postWrapper">
                <div className="postTop">
                   <div className="postTopLeft">
-                     <img src="" alt="" className="postProfileImg" />
-                     <span className="postUsername">Axel Olivas</span>
-                     <span className="postDate">5 min ago</span>
+                     <img src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                     <span className="postUsername">{Users.filter(u => u.id === post.userId)[0].username}</span>
+                     <span className="postDate">{post.date}</span>
                   </div>
                   <div className="postTopRight">
                      <MoreVert />
                   </div>
                </div>
                <div className="postCenter">
-                  <span className="postText">Hello world! My first post</span>
+                  <span className="postText">{post?.desc}</span>
+                  <img src={post.photo} alt="" className="postImg" />
                </div>
                <div className="postBottom">
                   <div className="postBottomLeft">
-                     {/* Like Icons and Counter */}
+                     <span className="postLikeCounter">{post.like} people like it</span>
                   </div>
                   <div className="postBottomRight">
-                     <span className="postComments">9 comments</span>
+                     <span className="postComments">{post.comment} comments</span>
                   </div>
                </div>
             </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,52 @@ import './rightbar.css'
 export default function Rightbar() {
    return (
       <div className='rightbar'>
-         rightbar
+         <div className="rightbarWrapper">
+            <div className="birthdayContainer">
+               <img src="" alt="" className="birthdayImg" />
+               <span className="birthdayText">
+                  <b>Jane Doe</b> and <b>3 other friends</b> have a birthday today
+               </span>
+            </div>
+            <h4 className="rightbarTitle">Online Friends</h4>
+            <ul className="rightbarFriendList">
+               <li className="rightbarFriend">
+                  <div className="rightbarProfileContainer">
+                     <img src="" alt="" className="rightbarProfileImg" />
+                     <span className="rightbarOnline"></span>
+                  </div>
+                  <span className="rightbarUsername">John Carter</span>
+               </li>
+               <li className="rightbarFriend">
+                  <div className="rightbarProfileContainer">
+                     <img src="" alt="" className="rightbarProfileImg" />
+                     <span className="rightbarOnline"></span>
+                  </div>
+                  <span className="rightbarUsername">John Carter</span>
+               </li>
+               <li className="rightbarFriend">
+                  <div className="rightbarProfileContainer">
+                     <img src="" alt="" className="rightbarProfileImg" />
+                     <span className="rightbarOnline"></span>
+                  </div>
+                  <span className="rightbarUsername">John Carter</span>
+               </li>
+               <li className="rightbarFriend">
+                  <div className="rightbarProfileContainer">
+                     <img src="" alt="" className="rightbarProfileImg" />
+                     <span className="rightbarOnline"></span>
+                  </div>
+                  <span className="rightbarUsername">John Carter</span>
+               </li>
+               <li className="rightbarFriend">
+                  <div className="rightbarProfileContainer">
+                     <img src="" alt="" className="rightbarProfileImg" />
+                     <span className="rightbarOnline"></span>
+                  </div>
+                  <span className="rightbarUsername">John Carter</span>
+               </li>
+            </ul>
+         </div>
       </div>
    )
 }
